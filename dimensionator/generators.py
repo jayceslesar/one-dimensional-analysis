67fab00726576966ed6d48bdcb3948668e6cd0ad
jayce <jslesar@beta.team>
2021-11-07 15:35:04 -0500

    some graphing

--- dimensionator/generators.py
+++ dimensionator/generators.py
@@ -2,6 +2,17 @@ import numpy as np
 import plotly.graph_objs as go
 import plotly.express as px
 from typing import Union
+import os
+import webbrowser
+
+
+def figures_to_html(figs, filename="dashboard.html"):
+    dashboard = open(filename, 'w')
+    dashboard.write("<html><head></head><body>" + "\n")
+    for fig in figs:
+        inner_html = fig.to_html().split('<body>')[1].split('</body>')[0]
+        dashboard.write(inner_html)
+    dashboard.write("</body></html>" + "\n")
 
 
 class DataGen:
@@ -11,12 +22,14 @@ class DataGen:
             np.random.seed(seed)
 
         self.num_points = num_points
+        self.plot = None
+        self.rate_plot = None
 
     def generate(self):
         """Generate some time series data for use."""
         out = np.zeros(self.num_points)
 
-        self.num_sections = np.random.randint(3, 8)
+        self.num_sections = np.random.randint(3, 10)
 
         done = False
         points_left_to_generate = self.num_points
@@ -41,7 +54,6 @@ class DataGen:
                     if posneg == 1:
                         generated_point = out[index - 1] + section_change_per_point
 
-                    print(posneg, generated_point)
                     out[index] = generated_point
                     index += 1
 
@@ -50,16 +62,55 @@ class DataGen:
 
         self.data = out
 
-    def plot(self):
+    def normal_plot(self):
         fig = go.Figure()
 
         fig.add_trace(go.Scatter(x=np.arange(len(self.data)), y=self.data, mode='lines'))
         fig.update_xaxes(title='Timestamp (index)')
         fig.update_yaxes(title='Value')
-        fig.update_layout(title=f'Data of size {self.num_points} and {self.num_sections} sections.')
-        fig.show()
+        fig.update_layout(title=f'Data of size {self.num_points:,} and {self.num_sections} sections.')
 
-    def analyze(self):
+        self.plot = fig
+
+    def find_rates_by_diff(self):
+        data_index_by_rate = {}
         diffs = np.abs(np.diff(self.data))
-        number_of_rates = set(diffs)
-        print(number_of_rates)
+        rates = list(set(diffs))
+
+        for rate in rates:
+            indexes_at_rate = np.where(np.abs(np.diff(self.data)) == rate)[0]
+            data_index_by_rate[rate] = (indexes_at_rate[0], indexes_at_rate[-1])
+
+        bounds = [data_index_by_rate[bound] for bound in data_index_by_rate.keys()]
+        sorted_bounds = sorted(bounds, key=lambda x: x[0])
+
+        inv_data_index_by_rate = {v: k for k, v in data_index_by_rate.items()}
+        sorted_rates = []
+        for bound in sorted_bounds:
+            sorted_rates.append(inv_data_index_by_rate[bound])
+
+        fig = go.Figure()
+        for i, bound in enumerate(sorted_bounds):
+            start = bound[0]
+            end = bound[1]
+            x = np.arange(start, end)
+            y = self.data[start:end]
+
+            fig.add_trace(go.Scatter(x=x, y=y, mode='lines', name=f'Average Change of {sorted_rates[i]}'))
+
+        fig.update_xaxes(title='Timestamp (index)')
+        fig.update_yaxes(title='Value')
+        fig.update_layout(title=f'Data of size {self.num_points:,} and {self.num_sections} sections.')
+
+        self.rate_plot = fig
+
+    def explore(self):
+        if self.plot is None:
+            self.normal_plot()
+        if self.rate_plot is None:
+            self.find_rates_by_diff()
+
+        to_plot = [self.plot, self.rate_plot]
+        path = os.path.join('explorable.html')
+        figures_to_html(to_plot, path)
+        webbrowser.open_new_tab(f'file://{os.path.join(os.getcwd(), path)}')
--- main.py
+++ main.py
@@ -2,15 +2,11 @@ from dimensionator.generators import DataGen
 
 
 def main():
-    my_data = DataGen(num_points=2000)
+    my_data = DataGen(num_points=500_000)
 
     my_data.generate()
 
-    print(len(my_data.data))
-
-    my_data.plot()
-
-    my_data.analyze()
+    my_data.explore()
 
 
 if __name__ == '__main__':
