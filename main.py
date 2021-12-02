a4f704f2e493d21010df2d24c9f20e2d7ee7418a
Cameron Hudson <camphudson@gmail.com>
2021-12-01 23:06:03 -0500

    Testing Simulations for Paper use

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+from numpy import number
 from dimensionator.generators import DataGen, DDDRandomWalk
-from networks import erdos_renyi, rw, draw, sub_graph, comparison_graph, run_multiple_runners, plt
+from networks import erdos_renyi, rw, draw, sub_graph, comparison_graph, run_multiple_runners, plt, random
 
 def main():
     # my_data = DataGen(num_points=500_000)
@@ -18,9 +19,9 @@ def networks():
     
     # running single walker
 
-    # length_of_walk = 1000
+    # length_of_walk = 100
     # # random walk will start at 0
-    # start = 0
+    # start = random.randrange(0, G.number_of_nodes())
     # visited = rw(G, length_of_walk, start)
     # draw(G)
     # plt.show()
@@ -30,16 +31,16 @@ def networks():
     # plt.show()
     # comparison_graph(G, H)
     
-    # running more than one walker
-    number_of_runners = 1
-    visited = run_multiple_runners(G, number_of_runners)
+    #running more than one walker
+    number_of_runners = 5
+    visited = run_multiple_runners(G, number_of_runners, walk_length=50)
     H = sub_graph(G, visited)
     draw(G) 
     plt.show()
     draw(H)
     plt.show()
 
-    comparison_graph(G, H)
+    comparison_graph(G, H, number_of_runners)
 
 if __name__ == '__main__':
     #main()
--- networks.py
+++ networks.py
@@ -7,6 +7,7 @@ import plotly.graph_objects as px
 import plotly.graph_objs as go
 import itertools
 
+
 def make_graph(prob, num_nodes):
   G = nx.DiGraph()
   p = prob
@@ -20,8 +21,10 @@ def make_graph(prob, num_nodes):
           G.add_edge(*(neighbor, node))
   return G
 
+
 def draw(graph):
   nx.draw(graph, with_labels=True, node_size=200, pos=nx.fruchterman_reingold_layout(graph))
+
 
 def metrics(graph):
   degree_centrality = nx.degree_centrality(graph)
@@ -33,11 +36,14 @@ def metrics(graph):
 
   return degree_centrality, in_degree_centrality, out_degree_centrality, closeness_centrality, between_centrality
 
+
 def erdos_renyi(num_nodes, p, directed):
   F = nx.fast_gnp_random_graph(num_nodes, p, directed=directed)
   return F
 
+
 def rw(graph, walk_length, starting_node):
+  # getting the adjecency matrix
   A = nx.adjacency_matrix(graph)
   A = A.todense()
   A = np.array(A, dtype = np.float64)
@@ -46,7 +52,6 @@ def rw(graph, walk_length, starting_node):
   D = np.diag(np.sum(A, axis=0))
 
   # transition matrix T
-  #T = np.dot(np.linalg.inv(D), A)
   T = A / A.sum(axis=1, keepdims=True)
 
   # random walk length
@@ -58,54 +63,56 @@ def rw(graph, walk_length, starting_node):
 
   visited = list()
   for k in range(walkLength):
-      # evaluate the next state vector
       p = np.dot(T,p)
-      # choose the node with higher probability as the visited node
       visited.append(np.argmax(p))
   return visited
+
 
 def sub_graph(graph, visited):
   H = nx.subgraph(graph, visited)
   return H
 
+
 def dict_mean(dicts):
   return sum(dicts.values()) / len(dicts)
 
-def comparison_graph(G, H):
 
+def comparison_graph(G, H, num_walkers):
   Gdc, Gidc, Godc, Gcc, Gbc  = metrics(G)
   Hdc, Hidc, Hodc, Hcc, Hbc = metrics(H)
   # creating random data through randomint
   # function of numpy.random
   np.random.seed(42)
 
-  x = ['Degree Centrality', 'In-Degree Centrality', 'Out-degree Centrality', 'Closeness Centrality', 'Betweeness Centrality']
+  x = ['Degree Centrality', 'In-Degree Centrality', 'Out-degree Centrality', 'Closeness Centrality', 'Betweenness Centrality']
 
   plot = px.Figure(data=[go.Bar(
       name = 'Entire Graph',
       x = x,
       y = [dict_mean(Gdc), dict_mean(Gidc), dict_mean(Godc), dict_mean(Gcc), dict_mean(Gbc)],
-      text = [round(dict_mean(Gdc), 3), round(dict_mean(Gidc), 3), round(dict_mean(Godc), 3), round(dict_mean(Gcc), 3), round(dict_mean(Gbc), 3)],
+      text = [round(dict_mean(Gdc), 3), round(dict_mean(Gidc), 3), round(dict_mean(Godc), 3),
+             round(dict_mean(Gcc), 3), round(dict_mean(Gbc), 3)],
       textposition='auto'
     ),
                         go.Bar(
       name = 'Sub-Graph',
       x = x,
       y = [dict_mean(Hdc), dict_mean(Hidc), dict_mean(Hodc), dict_mean(Hcc), dict_mean(Hbc)],
-      text = [round(dict_mean(Hdc), 3), round(dict_mean(Hidc), 3), round(dict_mean(Hodc), 3), round(dict_mean(Hcc), 3), round(dict_mean(Hbc), 3)],
+      text = [round(dict_mean(Hdc), 3), round(dict_mean(Hidc), 3), round(dict_mean(Hodc), 3),
+             round(dict_mean(Hcc), 3), round(dict_mean(Hbc), 3)],
       textposition='auto'
     )
   ])
   
-  plot.update_layout(title='Metric Comparison of Graph vs. Sub-Graph')
+  plot.update_layout(title=f'Metric Comparison of Graph with {G.number_of_nodes()} nodes vs. Sub-Graph with {H.number_of_nodes()} nodes with {num_walkers} walkers')
                     
   plot.show()
 
 
-def run_multiple_runners(graph, num_times):
+def run_multiple_runners(graph, num_times, walk_length):
   l = {}
   for _ in range(num_times):
     rand_node = random.randrange(0, graph.number_of_nodes())
-    visited = rw(graph, 100, rand_node)
+    visited = rw(graph, walk_length, rand_node)
     l[rand_node] = visited
   return set(itertools.chain(*list(l.values())))
